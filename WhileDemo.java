a7feba0007d8382ecb2c6bba93a8a7a521291403
kanu0158 <kanu0158@naver.com>
2018-05-30 10:37:32 +0900

    if_switch 수정

--- WhileDemo.java
+++ WhileDemo.java
@@ -1,9 +1,20 @@
 public class WhileDemo{
 	public static void main(String[] args){
 		int count = 1;
+		String op = " + ";
+		String result ="";
+		int sum = 0;
+
 		while(count < 11){
-			System.out.print(count + " + ");
+			sum += count;
+			result += count ;
+			if(count == 10){
+				op = " = ";
+			}
+			result += op;
 			count++;
 		}
+		System.out.print(result + sum);
+
 	}
 }
